656cba9508ef204c176adf10ff2bcbbb12fad49f
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-10-11 19:41:26 +0500

    Initial commit

---
+++ AudioMerger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace AudioMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
